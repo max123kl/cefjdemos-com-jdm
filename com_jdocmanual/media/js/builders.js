7ab883bc253fa857c5ad20d2faad0b44a8dae7e0
Cliff Ford <cliff@ford.myzen.co.uk>
2024-09-19 09:05:25 +0100

    Added Wait msg to Sources build.

--- com_jdocmanual/media/js/builders.js
+++ com_jdocmanual/media/js/builders.js
@@ -5,11 +5,10 @@
 let updateHTML = function(event) {
 	event.preventDefault();
 	if(confirm('On first use this action may take several minutes. On rebuild it should be quick.')) {
-		// If there is an alert open - find and close it.
-		if (false) {
-			this.dispatchEvent(new CustomEvent('joomla.alert.close'));
-			this.style.animationName = 'joomla-alert-fade-out';
-		}
+		// Set an alert message using the system message container.
+		const elem = document.getElementById('system-message-container');
+		elem.innerHTML = '<div class="alert alert-info text-center">Please Wait!</div>';
+		window.scrollTo(0, 0);
 		let url = '?option=com_jdocmanual&task=sources.buildhtml&manual=' + this.id + '&language=' + this.value;
 		location = url;
 	}
